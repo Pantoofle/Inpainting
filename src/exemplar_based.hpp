69452fdaf8a45cc19a1734aefd4819d65a520ad3
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2018-04-23 23:38:34 +0200

    Update headers

--- src/exemplar_based.hpp
+++ src/exemplar_based.hpp
@@ -17,13 +17,16 @@
 
 using namespace cimg_library;
 
+enum class Pxl_stat : char { Known, Filled, Frontiere, Empty };
+
 void ex_based_inpainting(const char *, const char *, const char *, int);
 
 double norm(std::pair<double, double> v);
-std::pair<double, double> vect_prod(std::pair<double, double> a,
-                                    std::pair<double, double> b);
+double vect_scal(std::pair<double, double> a, std::pair<double, double> b);
 
-std::pair<double, double> grad(CImg<int> &, int, int);
-std::pair<double, double> normal_vect(CImg<int> &, int, int);
+std::pair<double, double>
+grad(CImg<int> &, std::vector<std::vector<Pxl_stat>> &, int, int, int);
+std::pair<double, double> normal_vect(std::vector<std::vector<Pxl_stat>> &, int,
+                                      int, int);
 
 #endif // exemplar_based_hpp_INCLUDED
